cefa9e022fec1a5bbb3ec5bc679e51e1bc6db8d6
Kilte Leichnam <nwotnbm@gmail.com>
2014-08-31 16:17:21 +0400

    Updated MySQLi test

--- source/Tests/MySQLiTest.php
+++ source/Tests/MySQLiTest.php
@@ -19,13 +19,31 @@ use Kilte\MySQLi\MySQLi;
 class MySQLiTest extends MySQLiTestCase
 {
 
-    public function testQueryThrowsException()
+    /**
+     * Returns MySQLi instance
+     *
+     * @return MySQLi
+     */
+    protected function getMySQLi()
     {
         $config = $this->getMySQLiConfiguration();
         $db = new MySQLi($config['host'], $config['username'], $config['password'], $config['database']);
         $db->set_charset($config['charset']);
+
+        return $db;
+    }
+
+    public function testQueryThrowsException()
+    {
+        $db = $this->getMySQLi();
         $this->setExpectedException('Kilte\\MySQLi\\Exception\\MySQLiException');
         $db->query("INVALID QUERY");
+    }
+
+    public function testQueryReturnsResult()
+    {
+        $db = $this->getMySQLi();
+        $this->assertInstanceOf('\\mysqli_result', $db->query("SHOW TABLES"));
     }
 
 }
